72e164a0bd38cb7fe59df69e087ffae72855edbc
chimingkuei <chimingkuei@gmail.com>
2023-11-20 21:40:24 +0800

    SMC

--- SMC/obj/x64/Debug/SMC_Content.g.i.cs
+++ SMC/obj/x64/Debug/SMC_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("deepwise.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("ui/radiobuttonnoicon.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("ui/radiobuttonyesicon.png")]
 
 
